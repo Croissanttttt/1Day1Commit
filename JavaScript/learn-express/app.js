c55e76e12a1f80f1e5abae16ccde47f6a21b32b8
Croissanttttt <joyson1125@hanmail.net>
2020-01-08 16:53:59 +0900

    JS 알바가기전에 잠시 익스프레스

--- JavaScript/learn-express/app.js
+++ JavaScript/learn-express/app.js
@@ -13,7 +13,34 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+/*app.use(function(req, res, next) {
+  console.log(req.url, '저도 미들웨어입니다');
+  next();
+});*/
+
+app.use('/', function(req, res, next) {
+  console.log('첫 번째 미들웨어');
+  next();
+}, function(req, res, next) {
+  console.log('두 번째 미들웨어');
+  next();
+}, function(req, res, next) {
+  console.log('세 번째 미들웨어');
+  next();
+});
+
+/*app.use(function(req, res, next) {
+  if (Date.now() % 2 === 0) {
+    return res.status(404).send('50% 실패');
+  } else {
+    next();
+  }
+}, function(req, res, next) {
+  console.log('50% 성공');
+  next();
+});*/
+
+app.use(logger('dev'));// dev, short : 개발시 , common, combined : 배포시
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
